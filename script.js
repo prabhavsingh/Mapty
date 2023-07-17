3d162158bc07c96ef046aa8e829b7fb4d25a7390
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-17 20:04:29 +0530

    Using Geolocation API

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},`);
+    },
+    function () {
+      alert("could not get your position");
+    }
+  );
+}
