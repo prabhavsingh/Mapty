131018d878bdb9316ed452acae61336043dc77ce
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-18 11:54:00 +0530

    Working with localStorage

--- script.js
+++ script.js
@@ -65,6 +65,7 @@ class App {
   #workout = [];
   constructor() {
     this._getPosition();
+    this._getLocalStoage();
     form.addEventListener("submit", this._newWorkout.bind(this));
     inputType.addEventListener("change", this._toggleElevationField);
     containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
@@ -93,6 +94,9 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on("click", this._showForm.bind(this));
+    this.#workout.forEach((work) => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -172,6 +176,9 @@ class App {
 
     //clear input fields
     this._hideForm();
+
+    //set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -258,6 +265,27 @@ class App {
         duration: 1,
       },
     });
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem("workouts", JSON.stringify(this.#workout));
+  }
+
+  _getLocalStoage() {
+    const data = JSON.stringify(localStorage.getItem("workout"));
+
+    if (!data) return;
+
+    this.#workout = data;
+
+    this.#workout.forEach((work) => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem("workout");
+    location.reload();
   }
 }
 
